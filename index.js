8a7e373a34eb27e2e586fbbfb9ad807905badffa
primeempire12 <167495724+primeempire12@users.noreply.github.com>
2024-05-26 20:49:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'open.spotify.com') {
+    if (req.headers.host === 'croxyproxy.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
